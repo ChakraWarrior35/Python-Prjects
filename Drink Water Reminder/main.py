f5a741f764d9984016f2dc98b4e1989e5a6824c7
Abhishek Mondal <mandal.abhishek2045@gmail.com>
2026-01-09 18:51:02 +0530

    main.py

---
+++ Drink Water Reminder/main.py
@@ -0,0 +1,8 @@
+import time
+
+from plyer import notification
+ 
+while True:
+    
+    notification.notify(title= "Drink some Water",message = "We need to Drink some water ")
+    time.sleep(60*60)
