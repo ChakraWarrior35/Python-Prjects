344aaa1ec8b845add5539bd322eb81f601ef3cd0
Abhishek Mondal <mandal.abhishek2045@gmail.com>
2026-01-09 18:51:12 +0530

    update

--- odd or even.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Enter the number to check:"))
-if(a%2==0):
-    print("The number is odd")
-else:
-    print("The Number is Even")
--- reverse.py
+++
@@ -1,7 +0,0 @@
-#reverese the two digit number 
-a=input("Enter the number :")
-a=int(a)
-b=a//10
-c=a%10
-rev=c*10+b
-print("The reverse number is:",rev)
